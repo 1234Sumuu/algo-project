63ec6fd9e9c4f12337ad638cd07a49503a2a8e15
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2018-12-16 02:26:56 -0800

    Update pr01.c

--- pr01.c
+++ pr01.c
@@ -4,7 +4,7 @@
 int main()
 {
    int a[200000],b[200000],c[200000],d[200000],result[200000],j,n;
-   int temp1=0,t,k=0,temp,sum=0,t1,t2,temp2=0,s,x,y,i;
+   int i,temp;
 
  scanf("%d",&t);
  while(t>0)
@@ -19,11 +19,11 @@ int main()
        scanf("%d",&d[i]);
 
    }
-   x=abs(c[0]-a[0]);
+  
        temp1=x;
    for(i=1;i<n;i++)
    {
-       x=abs(c[i]-a[i]);
+       
        if(x!=temp1)
        {    temp1=200000;
 
@@ -31,32 +31,12 @@ int main()
        }
 
    }
-   y=abs(b[0]-d[0]);
-       temp2=y;
+   
    for(i=1;i<n;i++)
    {
-       y=abs(b[i]-d[i]);
-        if(y!=temp2)
-       {    temp2=200000;
-                 break;
-       }
+       
  }
-    printf("temp %d*temp2 %d*",temp1,temp2);
-
-   if(temp1!=200000 || temp2!=200000)
-   {
-       for(i=0;i<n;i++)
-    {
-        for(j=i;j<n;j++)
-        {
-            if(a[i]>a[j])
-            {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
+   
 
    for(i=0;i<n;i++)
     {
@@ -70,13 +50,7 @@ int main()
             }
         }
     }
-    s=(n/2 );
-    temp1=a[s];
-       for(i=0;i<n;i++)
-    {
-        sum=sum+(abs(a[s]-a[i]));
-       }
-      temp1=d[s];
+   
     for(i=0;i<n;i++)
     {
         sum=sum+(abs(d[s]-d[i]));
